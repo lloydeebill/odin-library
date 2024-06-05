2b32f5551ed8aa226d46fe236db5873053d1faa9
Lloyd Bill <lloydbill.lariosa@gmail.com>
2024-06-05 15:44:15 +0800

    added displayBook function which will loop forEachbook in myLibrary and outputs to the div using dom

--- library.js
+++ library.js
@@ -9,32 +9,58 @@ function Book(title,author,pages,info){
   this.info = info;
 }
 
-// const book1 = new Book("Man's Search for Meaning","Victor Frankl",200,"Frankl's Man's Search for Meaning provides a vivid account of an individual's experience as a prisoner in a Nazi concentration camp. The book focuses on love, hope, responsibility, inner freedom, and the beauty to be found in both nature and art as means that help one endure and overcome harrowing experiences.");
-
-
-
-// let bookTitle = document.querySelector(".book-title");
-// let bookAuthor = document.querySelector(".book-author");
-// let bookPages = document.querySelector(".book-pages");
-// let bookInfo = document.querySelector(".book-info");
-
-
-
-// bookTitle.innerText = `${book1.title}`
-// bookAuthor.innerText = `${book1.author}`
-// bookPages.innerText = `${book1.pages}`
-// bookInfo.innerText = `${book1.info}`
-
-
-
 
 function addBookToLibrary(title,author,pages,info) {
   const newBook = new Book(title,author,pages,info);
-  
   myLibrary.push(newBook);
+
+  displayBook();
 
 }
 
+function displayBook() {
+
+  //for each book, it will access each property//
+  //display property in a div inside the main content//
+  //change the html on the main content to accomodate for each div books//
+
+  const bookShelf = document.querySelector('.main');
+  bookShelf.innertText = '';
+
+  myLibrary.forEach((book) => {
+
+    const bookContainer = document.createElement('div');
+    bookContainer.classList.add('book-container');
+
+    const bookTitle = document.createElement('h2');
+    bookTitle.innerText = `${book.title}`;
+    bookContainer.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement('p');
+    bookAuthor.innerText = `${book.author}`;
+    bookContainer.appendChild(bookAuthor);
+
+    const bookPages = document.createElement('p');
+    bookPages.innerText = `${book.pages}`;
+    bookContainer.appendChild(bookPages);
+
+    const bookInfo = document.createElement('p');
+    bookInfo.innerText = `${book.info}`;
+    bookContainer.appendChild(bookInfo);
+    
+    
+
+
+    
+    
+
+    bookShelf.appendChild(bookContainer);
+
+  })
+}
+
+
+
 addBookToLibrary("Man's Search for Meaning","Victor Frankl",200,"Frankl's Man's Search for Meaning provides a vivid account of an individual's experience as a prisoner in a Nazi concentration camp. The book focuses on love, hope, responsibility, inner freedom, and the beauty to be found in both nature and art as means that help one endure and overcome harrowing experiences.");
 
-console.log(typeof(myLibrary[0]));
+console.log(myLibrary);
