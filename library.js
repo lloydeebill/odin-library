51ce4e0ead422925e99862daec94552a6b914388
Lloyd Bill <lloydbill.lariosa@gmail.com>
2024-06-05 16:12:06 +0800

    added some css improvements on how the page looks; changed the columns of bookshelf

--- library.js
+++ library.js
@@ -11,6 +11,7 @@ function Book(title,author,pages,info){
 
 
 function addBookToLibrary(title,author,pages,info) {
+  
   const newBook = new Book(title,author,pages,info);
   myLibrary.push(newBook);
 
@@ -25,7 +26,7 @@ function displayBook() {
   //change the html on the main content to accomodate for each div books//
 
   const bookShelf = document.querySelector('.main');
-  bookShelf.innertText = '';
+  bookShelf.innerText = '';
 
   myLibrary.forEach((book) => {
 
@@ -34,24 +35,23 @@ function displayBook() {
 
     const bookTitle = document.createElement('h2');
     bookTitle.innerText = `${book.title}`;
+    bookContainer.classList.add('book-title');
     bookContainer.appendChild(bookTitle);
 
     const bookAuthor = document.createElement('p');
     bookAuthor.innerText = `${book.author}`;
+    bookAuthor.classList.add('book-author');
     bookContainer.appendChild(bookAuthor);
 
     const bookPages = document.createElement('p');
     bookPages.innerText = `${book.pages}`;
+    bookPages.classList.add('book-pages');
     bookContainer.appendChild(bookPages);
 
     const bookInfo = document.createElement('p');
     bookInfo.innerText = `${book.info}`;
+    bookInfo.classList.add('book-info');
     bookContainer.appendChild(bookInfo);
-    
-    
-
-
-    
     
 
     bookShelf.appendChild(bookContainer);
@@ -62,5 +62,9 @@ function displayBook() {
 
 
 addBookToLibrary("Man's Search for Meaning","Victor Frankl",200,"Frankl's Man's Search for Meaning provides a vivid account of an individual's experience as a prisoner in a Nazi concentration camp. The book focuses on love, hope, responsibility, inner freedom, and the beauty to be found in both nature and art as means that help one endure and overcome harrowing experiences.");
+
+addBookToLibrary("Man's Search for Meaning","Victor Frankl",200,"Frankl's Man's Search for Meaning provides a vivid account of an individual's experience as a prisoner in a Nazi concentration camp. The book focuses on love, hope, responsibility, inner freedom, and the beauty to be found in both nature and art as means that help one endure and overcome harrowing experiences.");
+
+
 
 console.log(myLibrary);
